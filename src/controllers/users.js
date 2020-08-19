3a9a6facd7f81234bddd98166d58b1dfe73ddd81
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-08-19 11:36:14 -0500

    Adding response status to users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,3 +1,5 @@
+// GET //
+
 export const getEmployees = (token) => {
     return fetch('http://localhost:3000/users', {
       method: 'GET',
@@ -6,8 +8,21 @@ export const getEmployees = (token) => {
         'Authorization': `Bearer ${token}`,
         }
     })
-    .then(response => (response.json()))
+    .then((response) => {
+      switch (response.status) {
+        case 200:
+          return response.json();
+        case 401:
+          return Error('No hay cabecera de autenticación');
+        case 403:
+          return Error('El usuario loggeado no es admin');
+        default:
+          break;
+      }
+    })
 }
+
+// POST -> ADD //
 
 export const addEmployee = (token, data) => {
     return fetch('http://localhost:3000/users', {
@@ -18,8 +33,23 @@ export const addEmployee = (token, data) => {
         'Authorization': `Bearer ${token}`,
         }
     })
-    .then(response => (response.json()))
+    .then((response) => {
+      switch (response.status) {
+        case 200:
+          return response.json();
+        case 400:
+          return Error('You have not provided a valid email and or password');
+        case 401:
+          return Error('No hay cabecera de autenticación');
+        case 403:
+          return Error('El email ya se usó anteriormente');
+        default:
+          break;
+      }
+    })
 }
+
+// PUT -> EDIT //
 
 export const editEmployee = (token, data) => {
   return fetch('http://localhost:3000/users', {
@@ -30,8 +60,25 @@ export const editEmployee = (token, data) => {
       'Authorization': `Bearer ${token}`,
       }
   })
-  .then(response => (response.json()))
+  .then((response) => {
+    switch (response.status) {
+      case 200:
+        return response.json();
+      case 400:
+        return Error('You have not provided a valid email and or password');
+      case 401:
+        return Error('No hay cabecera de autenticación');
+      case 403:
+        return Error('El usuario loggeado no es admin o no es la misma usuaria');
+      case 404:
+        return Error('El usuario no existe');
+      default:
+        break;
+    }
+  })
 }
+
+// DELETE //
 
 export const deleteEmployee = (token, id) => {
   return fetch(`http://localhost:3000/users/${id}`, {
@@ -41,5 +88,18 @@ export const deleteEmployee = (token, id) => {
       'Authorization': `Bearer ${token}`,
       }
   })
-  .then(response => (response.json()))
+  .then((response) => {
+    switch (response.status) {
+      case 200:
+        return response.json();
+      case 401:
+        return Error('No hay cabecera de autenticación');
+      case 403:
+        return Error('El usuario loggeado no es admin o no es la misma usuaria');
+      case 404:
+        return Error('El usuario no existe');
+      default:
+        break;
+    }
+  })
 }
