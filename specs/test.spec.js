685e819599907e384708d9f025eee130c9e08364
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-10-08 16:48:15 -0500

    Installation problem fixed

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -1,20 +1,5 @@
-// Import the `mount()` method from Vue Test Utils
-import { mount } from '@vue/test-utils'
+import EmployeeList from '../src/components/EmployeeList.vue'
 
-// The component to test
-const MessageComponent = {
-  template: '<p>{{ msg }}</p>',
-  props: ['msg']
-}
-
-test('displays message', () => {
-  // mount() returns a wrapped Vue component we can interact with
-  const wrapper = mount(MessageComponent, {
-    propsData: {
-      msg: 'Hello world'
-    }
-  })
-
-  // Assert the rendered text of the component
-  expect(wrapper.text()).toContain('Hello world')
+test('first spec', () => {
+  console.log(EmployeeList)
 })
